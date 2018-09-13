4b4bff8461de5e6a95a538195008d2f5eff3213b
roxas <genesigzero@naver.com>
2018-09-13 16:59:10 +0900

    login 기능추가

--- board/index.js
+++ board/index.js
@@ -6,6 +6,7 @@ var bodyParser     = require("body-parser");
 var methodOverride = require("method-override");
 var flash          = require("connect-flash"); // 1
 var session        = require("express-session"); // 1
+var passport       = require("./config/passport");
 var app = express();
 
 // DB setting
@@ -26,6 +27,17 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.use(flash()); // 2
 app.use(session({secret:"MySecret", resave:true, saveUninitialized:true})); // 3
+
+// Passport // 2
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Custom Middlewares // 3
+app.use(function(req,res,next){
+ res.locals.isAuthenticated = req.isAuthenticated();
+ res.locals.currentUser = req.user;
+ next();
+})
 
 // Routes
 app.use("/", require("./routes/home"));
